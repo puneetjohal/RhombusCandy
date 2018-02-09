816f1be3e93a936330752ba377fab775f8088e3f
Puneet Johal <pjohal@stuy.edu>
2018-02-09 13:16:45 -0500

    fixed driver a little but why is it constant

--- MFDriver.java
+++ MFDriver.java
@@ -2,29 +2,37 @@ public class MFDriver {
 
   //populates matricies with random integer values between 0 and 1000
   public static void populate (int[][] arr) {
+    int cntr = 0;
     for (int r = 0; r < arr.length; r++) {
       for (int c = 0; c < arr.length; c++) {
-        arr[r][c] = (int)(Math.random() * 1000);
+        arr[r][c] = cntr;
+        cntr++;
       }
     }
+  }
+
+  public static long getTime (int[][] arr) {
+    MatrixFinder test = new MatrixFinder();
+    int worstCase = arr[arr.length-1][0]; //worst case the value at the bottom left
+    long startTime = System.currentTimeMillis();
+    test.search( worstCase, arr );
+    long endTime = System.currentTimeMillis();
+    return endTime - startTime;
   }
 
   public static void main (String[] args) {
 
     //1 thousand trials
     for (int i = 0; i < 1000; i++) {
-      long startTime = System.currentTimeMillis();
       //for each trial test 100 different randomly generated matrices of the same length for the worst case
-      for (int x = 0; i < 100; x++) {
-        int[][] arr = new int[1000 + (i*100)][1000 + (i*100)];
+      int n = 1000 + (i*100); //n increases by 100 after each trial
+      long totalTime = 0;
+      for (int x = 0; x < 100; x++) {
+        int[][] arr = new int[n][n];
         populate(arr);
-        int worstCase = arr[arr.length-1][0]; //worst case the value at the bottom left
-        MatrixFinder test = new MatrixFinder();
-        test.search( worstCase, arr );
-        //note to self: creating + populating a new array affects the runtime
+        totalTime += getTime(arr);
       }
-      long endTime = System.currentTimeMillis();
-      System.out.println(endTime-startTime/100);
+      System.out.println( n + "," + totalTime/100 );
     }
 
   }
