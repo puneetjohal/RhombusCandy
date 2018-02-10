094d451fb9aa22dd4c47a3374d1b8760e8c87039
Puneet <pjohal@stuy.edu>
2018-02-10 11:52:29 -0500

    updated driver

--- MFDriver.java
+++ MFDriver.java
@@ -12,32 +12,32 @@ public class MFDriver {
   public static long getTime (int[][] arr) {
     MatrixFinder test = new MatrixFinder();
     int worstCase = arr[arr.length-1][0]; //worst case the value at the bottom left
-    long startTime = System.currentTimeMillis();
+    long startTime = System.nanoTime();
     test.search( worstCase, arr );
-    long endTime = System.currentTimeMillis();
+    long endTime = System.nanoTime();
     return endTime - startTime;
   }
 
   public static void main (String[] args) {
 
-    //10 "warm up" trials
-    for (int w = 0; w < 10; w++){
+    //100 "warm up" trials
+    for (int w = 0; w < 100; w++){
       int[][] foo = new int[1000][1000];
       populate(foo);
       getTime(foo);
     }
 
-    //1000 experimental trials
-    for (int i = 0; i < 1000; i++) {
-      //for each trial test search method on 100 different randomly generated matricies for the worst case (bottom left corner)
-      int n = 1000 + (i*100); //n increases by 100 after each trial
+    //360 experimental trials
+    for (int i = 0; i < 360; i++) {
+      //for each trial test search method on 100 different randomly generated matricies for the worst case (bottom left corner) --> 36,000 trials total
+      int n = 1000 + (i*25); //n increases by 25 after each trial
       long totalTime = 0;
       for (int x = 0; x < 100; x++) {
         int[][] arr = new int[n][n];
         populate(arr);
         totalTime += getTime(arr);
       }
-      System.out.println( n + "," + (double)totalTime/100 );
+      System.out.println( n + "," + totalTime/(long)100 );
     }
 
   }
